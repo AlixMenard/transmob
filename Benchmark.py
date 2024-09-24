50be40d296679f8f44a3c8ab38deaf62dd1eb510
AlixMenard <amenard@ec-m.fr>
2024-09-24 12:52:26 +0200

    Lighter benchmark

--- Benchmark.py
+++ Benchmark.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
 
     f = r"C:\Users\guest_l5dyhea\Desktop\transmob\videos\media2shortmult"
 
-    processes["Classique"], lines = vp.models_trials(f, cores =5)
+    processes["Classique"], lines = vp.models_trials(f, cores =4)
     processes["NT"] = vpNT.models_trials(f, 3, lines)
-    processes["YT"] = vpYT.models_trials(f, 5, lines)
+    processes["YT"] = vpYT.models_trials(f, 4, lines)
