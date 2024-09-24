7eb4d29219ad8ac07d7eec6370272904df627763
AlixMenard <amenard@ec-m.fr>
2024-09-24 16:34:25 +0200

    Fixed window text for YT

--- GUI.py
+++ GUI.py
@@ -33,7 +33,7 @@ def setup_window():
     process = tk.StringVar(value = "NT")
     T = tk.Radiobutton(root, text = "Classic", variable = process, value = "Classic")
     TNT = tk.Radiobutton(root, text = "Nested Threads", variable = process, value = "NT")
-    TYT = tk.Radiobutton(root, text = "YAllO"+" (CUDA)" if bcuda else "", variable = process, value = "YT")
+    TYT = tk.Radiobutton(root, text = "YAllO"+(" (CUDA)" if bcuda else ""), variable = process, value = "YT")
     T.grid(row = 0, column = 2, columnspan = 2, pady=20, padx=5)
     TNT.grid(row = 0, column = 4, columnspan = 2, pady=20, padx=5)
     TYT.grid(row = 0, column = 6, columnspan = 2, pady=20, padx=5)
