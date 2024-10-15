85674f4cfd0d5297effcd33f1f9fdfa516531aa1
AlixMenard <amenard@ec-m.fr>
2024-10-15 16:44:03 +0200

    updated classes names

--- TransmobNT/Analyze.py
+++ TransmobNT/Analyze.py
@@ -102,7 +102,7 @@ class Analyser:
         if verbose: print("Tracker loaded...")
         self.class_labels = [
             "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-            "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+            "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird",
             "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
             "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
             "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
