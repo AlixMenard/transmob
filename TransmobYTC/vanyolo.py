9febad98e9edaeb0cbb881ad5ce8c3e6c1e3e010
AlixMenard <amenard@ec-m.fr>
2024-10-22 11:08:18 +0200

    less memory usage

--- TransmobYTC/vanyolo.py
+++ TransmobYTC/vanyolo.py
@@ -3,4 +3,4 @@ import os
 
 model = YOLO(r"../weights/yolo11n.pt")
 os.chdir(os.getcwd())
-model.train(data="coco.yaml", epochs=100, imgsz=640, device=0)
+model.train(data="coco.yaml", epochs=100, imgsz=736, device=0, batch=0.8)
