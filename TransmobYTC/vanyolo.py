7eb2e1b45ebe144ec47d75274cbbd17353fc5983
Alix Menard <amenard@ec-m.fr>
2024-10-28 08:59:29 +0100

    train on x

--- TransmobYTC/vanyolo.py
+++ TransmobYTC/vanyolo.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 import os
 
 if __name__ == "__main__":
-    model = YOLO(r"../weights/yolo11n.pt")
+    model = YOLO(r"../weights/yolo11x.pt")
     os.chdir(os.getcwd())
     model.train(data="coco.yaml", epochs=100, imgsz=736, device=0, batch=0.8)
