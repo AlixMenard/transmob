156569d9ac0cc06fadbba61d70c274223a9d0a9c
AlixMenard <amenard@ec-m.fr>
2024-11-08 09:37:56 +0100

    Fixed one trust time ?

--- TransmobYTC/Analyze.py
+++ TransmobYTC/Analyze.py
@@ -197,6 +197,7 @@ class Analyser:
         chg_b = tk.Button(root, text="Changer", command=change)
         chg_b.grid(row=2, column=1)
         root.mainloop()
+        print("in get time : ", ret)
         return ret
 
     def process(self):
