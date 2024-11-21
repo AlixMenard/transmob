968b6f6f82df6aa3fef6d4d075df4223ea92e6ca
Alix Menard <amenard@ec-m.fr>
2024-11-21 11:31:39 +0100

    Train normal+van

--- TransmobYTC/vanyolo.py
+++ TransmobYTC/vanyolo.py
@@ -4,4 +4,4 @@ import os
 if __name__ == "__main__":
     model = YOLO(r"../weights/yolo11x.pt")
     os.chdir(os.getcwd())
-    model.train(data="coco.yaml", epochs=100, imgsz=736, device=0, batch=0.8)
+    model.train(data=r"coco.yaml", epochs=50, imgsz=640, device=0, batch=0.8)
