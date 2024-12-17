373ad65a23dc12a3d8ea368c537b0511f9a08655
AlixMenard <amenard@ec-m.fr>
2024-12-17 15:10:14 +0100

    Test reid on video

--- TransmobYTC/ReID.py
+++ TransmobYTC/ReID.py
@@ -7,6 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+from copy import deepcopy
 
 class ReId:
     def __init__(self, model = r"../FastReId_config/veriwild_bot_R50-ibn.pth", config = r"../FastReId_config/bagtricks_R50-ibn.yml"):
@@ -22,14 +23,20 @@ class ReId:
         predictions = self.predictor(image)
         return predictions
 
-    def proxi_matrix(self, folder):
-        cwd = os.getcwd()
-        os.chdir(folder)
-        files = os.listdir()
-        images = [cv2.imread(f) for f in files]
+    def proxi_matrix(self, folder, feat2 = None):
+        if type(folder) == str:
+            cwd = os.getcwd()
+            os.chdir(folder)
+            files = os.listdir()
+            images = [cv2.imread(f) for f in files]
+        else:
+            images = folder[:]
         features = [self(img) for img in images]
         feature_matrix = np.vstack(features)
-        feature_matrix = cosine_similarity(feature_matrix)
+        if feat2 in None:
+            feature_matrix = cosine_similarity(feature_matrix)
+        else:
+            feature_matrix = cosine_similarity(feature_matrix, feat2)
         os.chdir(cwd)
         return feature_matrix
 
@@ -44,3 +51,57 @@ class ReId:
         )
         plt.title("Proximity Matrix with Annotations")
         plt.show()
+
+if __name__ == '__main__':
+    from ultralytics import YOLO
+    model = YOLO(r"C:\Users\guest_l5dyhea\Desktop\transmob\weights\yolo11x.pt")
+    model.to("cuda")
+
+    reid = ReId()
+    t_features = []
+
+    cap = cv2.VideoCapture(r"C:\Users\guest_l5dyhea\Desktop\transmob\videos\media\Fait_Aix 1_15' _piétons.mp4")
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        results = yolo.track(frame, tracker="botsort.yaml", persist=True, verbose=False,
+                             device=0, conf=0.1)
+        try:
+            ids = results[0].boxes.id.int().cpu().tolist()
+        except:
+            continue
+        classes = results[0].boxes.cls.int().cpu().tolist()
+        confs = results[0].boxes.conf.float().cpu().tolist()
+        boxes = results[0].boxes.xyxy.cpu().tolist()
+
+        features = []
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box)
+            features.append(reid(frame[y1:y2, x1:x2]))
+        if t_features:
+            prox = reid.proxi_matrix(features, t_features[:, 1:])
+        reid_ranks = []
+        for i,id in enumerate(ids):
+            if not id in t_features[:, 0]:
+                continue
+            temp = prox[i]
+            ind = np.where(t_features[:, 0] == id)[0][0]
+            value = temp[ind]
+            temp = np.sort(temp)
+            rank = np.where(temp == value)[0][0]
+            reid_ranks.append(rank)
+
+        for id, classe, conf, box, rank in zip(ids, classes, confs, boxes, reid_ranks):
+            x1, y1, x2, y2 = map(int, box)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
+            v = self.fleet.get(id)
+            cv2.putText(frame, f'{id} ({rank})', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                        (0, 255, 0), 2)
+
+        ids = ids.reshape(-1,1)
+        t_features = deepcopy(features)
+        t_features = np.hstack((ids, t_features))
