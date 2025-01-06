a703663a0dd6eaa1d43f5c2e5d13c824c14ac546
AlixMenard <amenard@ec-m.fr>
2025-01-06 14:58:59 +0100

    video merging

--- tools/aggregateVideos.py
+++ tools/aggregateVideos.py
@@ -6,11 +6,13 @@ def aggregate(path, target):
     files = os.listdir(path)
     files = sorted(files, key=lambda t: os.stat(os.path.join(path,t)).st_mtime)
     lengths = [get_vid_length(os.path.join(path,f)) for f in files]
+    count = 0
     while files:
         temp = []
         while files and  abs(sum([t[1] for t in temp])-target*60) > abs(sum([t[1] for t in temp+[(files[0],lengths[0])]])-target*60):
             temp.append((files.pop(0),lengths.pop(0)))
-        merge(temp, path)
+        merge(temp, path, count)
+        count += 1
 
 def get_vid_length(path):
     video = cv2.VideoCapture(path)
@@ -19,7 +21,7 @@ def get_vid_length(path):
     duration = total_frames / fps
     return duration
 
-def merge(temp, path):
+def merge(temp, path, count):
     vids = [VideoFileClip(os.path.join(path,t[0])) for t in temp]
     n_vid = concatenate_videoclips(vids, method="compose")
-    n_vid.write_videofile(os.path.join(path,"merged.mp4"))
+    n_vid.write_videofile(os.path.join(path,f"merged_{count}.mp4"))
