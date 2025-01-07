1f6c17cac40e84071973091dac35c530a4e3f43f
AlixMenard <amenard@ec-m.fr>
2025-01-07 14:58:52 +0100

    ROI bounds slightly tighter

--- TransmobYTC/Line.py
+++ TransmobYTC/Line.py
@@ -54,7 +54,7 @@ class Line:
         self.mask_bound()
 
     def mask_bound(self):
-        margin = 3/5
+        margin = 7/145
         self.maskbound = [
             self.center[0] - self.length * margin,  # left
             self.center[1] - self.length * margin,  # top
