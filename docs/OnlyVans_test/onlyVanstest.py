d87ab015f66242b7159995af72a557f9c7225c66
AlixMenard <amenard@ec-m.fr>
2025-01-07 09:08:53 +0100

    onlyVans test

--- docs/OnlyVans_test/onlyVanstest.py
+++ docs/OnlyVans_test/onlyVanstest.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 import torch
 import numpy as np
 import cv2
+import json
+import os
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-import os
 
 #endRegion
 
@@ -24,6 +24,20 @@ def get_cars(coco_path):
             break
     return cars
 
+def get_trucks(coco_path):
+    labels = os.listdir(rf"{coco_path}/labels/train2017")
+    np.random.shuffle(labels)
+    trucks = []
+    for label in labels:
+        with open(os.path.join(rf"{coco_path}/labels/train2017", label), 'r') as f:
+            txt = f.read()
+        txt = txt.split(" ")[0]
+        if txt == "7":
+            trucks.append(os.path.join(rf"{coco_path}/images/train2017", label))
+        if len(trucks) > 50:
+            break
+    return trucks
+
 def get_vans(vans_path):
     images = os.listdir(rf"{vans_path}/train/images")
     np.random.shuffle(images)
@@ -37,33 +51,61 @@ def get_vans(vans_path):
 def fight(coco_path, vans_path, onlyvans, yolo11):
     cars = get_cars(coco_path)
     vans = get_vans(vans_path)
+    trucks = get_trucks(coco_path)
 
     model = YOLO(yolo11).to(device)
 
     cars_confs_yolo = []
     vans_confs_yolo = []
+    trucks_confs_yolo = []
+    print("cars yolo...")
     for car in cars:
         frame = cv2.imread(car)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         conf = model.detect(frame, classes = [2])[0].boxes.conf[0]
         cars_confs_yolo.append(conf)
+    print("vans yolo...")
     for van in vans:
         frame = cv2.imread(van)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        conf = model.detect(frame, classes = [2])[0].boxes.conf[0]
+        conf = model.detect(frame, classes = [2,7])[0].boxes.conf[0]
         vans_confs_yolo.append(conf)
+    print("trucks yolo...")
+    for truck in trucks:
+        frame = cv2.imread(truck)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        conf = model.detect(frame, classes = [7])[0].boxes.conf[0]
+        trucks_confs_yolo.append(conf)
 
     model = YOLO(onlyvans).to(device)
 
     cars_confs_vans = []
     vans_confs_vans = []
+    trucks_confs_vans = []
+    print("cars vans...")
     for car in cars:
         frame = cv2.imread(car)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         conf = model.detect(frame)[0].boxes.conf[0]
         cars_confs_vans.append(conf)
+    print("vans vans...")
     for van in vans:
         frame = cv2.imread(van)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         conf = model.detect(frame)[0].boxes.conf[0]
         vans_confs_vans.append(conf)
+    print("trucks vans...")
+    for truck in trucks:
+        frame = cv2.imread(truck)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        conf = model.detect(frame)[0].boxes.conf[0]
+        trucks_confs_vans.append(conf)
+
+    dic = {"cars_confs_yolo": cars_confs_yolo,
+           "vans_confs_yolo": vans_confs_yolo,
+           "trucks_confs_yolo": trucks_confs_yolo,
+           "cars_confs_vans": cars_confs_vans,
+           "vans_confs_vans": vans_confs_vans,
+           "trucks_confs_vans": trucks_confs_vans}
+    with open("test.json", "w") as f:
+        json.dump(dic, f)
