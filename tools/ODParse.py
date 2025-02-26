db2bfcc3b8983341d47ed07a12aa96d9bbfa6c8d
AlixMenard <amenard@ec-m.fr>
2025-02-26 12:41:16 +0100

    Rematched layout to number of propositions

--- tools/ODParse.py
+++ tools/ODParse.py
@@ -282,7 +282,7 @@ class Parser:
                 image = normalize(image).unsqueeze(0).to(cfg.MODEL.DEVICE)
                 vehicle.features = predictor(image)
         print("Matching...", flush = True)
-        id_matched = match_with_user_validation(vehicle_paths, root, top_k=10)
+        id_matched = match_with_user_validation(vehicle_paths, root, top_k=12)
         print(len(id_matched))
         for enter_id, exit_id, vehicle in id_matched:
             od_mat.directions[enter_id][exit_id].append(vehicle)
