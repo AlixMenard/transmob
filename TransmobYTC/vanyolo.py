0e20f11a8e8c0846bb6d3c7ffec81ba3f6c50dc0
AlixMenard <amenard@ec-m.fr>
2025-02-28 10:14:12 +0100

    useless files

--- TransmobYTC/vanyolo.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-import os
-
-if __name__ == "__main__":
-    model = YOLO(r"../weights/yolo11m.pt")
-    os.chdir(os.getcwd())
-    model.train(data=r"data.yaml", epochs=50, imgsz=640, device=0, batch=0.8)
